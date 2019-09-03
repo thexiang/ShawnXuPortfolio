c04467d04cdae0d498ed9c5894daacb0861110ca
thexiang <xxiang78@gmail.com>
2019-09-03 01:48:21 -0400

    set up

--- content/pages/about.md
+++
@@ -1,6 +0,0 @@
-Title: About Me
-Date: 2019-08-21 23:00
-Modified: 2019-08-21 23:00
-
-![](images/project1.png)
-test
--- shell_scripts/prod_build.sh
+++ shell_scripts/prod_build.sh
@@ -1,3 +1,2 @@
 source dev/bin/activate
-pelican content -s pelicanconf.py -t theme/sx_portfolio_thme
-aws s3 cp output/ s3://shawnxuportfolio/ --recursive
+aws s3 cp output/ s3://xiangxu.io/ --recursive
